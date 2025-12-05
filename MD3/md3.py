5d69c2c421cf5e09a1c24b53e65784c2699dbcfa
krjohnn <ks91ko@gmail.com>
2025-12-06 00:38:10 +0200

    Setting up template for MD3

--- MD3/md3.py
+++ MD3/md3.py
@@ -1,13 +1,16 @@
 import hashlib
 
 def gcd(a, b):
+    return
 
 def gcdExtended(a, b):
+    return
 
 def generate_keys(p, q):
+    return
 
 def main():
-    print("Biolerplate")
+    print("Start")
 
 if __name__ == "__main__":
     main()
