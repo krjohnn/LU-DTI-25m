c24fd8c5c76a8b490f1f20b1920942185aea7370
krjohnn <ks91ko@gmail.com>
2025-12-06 01:40:04 +0200

    Experimenting

--- MD3/md3.py
+++ MD3/md3.py
@@ -1,12 +1,13 @@
 import hashlib
 import math
+import random
 
 def gcd(a, b):
     while b != 0:
         a, b = b, a % b
     return a
 
-def gcdExtended(a, b):
+def gcdExtended(a, b) -> list:
     if a == 0:
         return b, 0, 1
     gcd, x1, y1 = gcdExtended(b % a, a)
@@ -19,9 +20,23 @@ def generate_keys(p, q):
 
 def main():
     print("Start")
-    print(gcd(24,36))
-    print(gcdExtended(24,36))
-    print(math.gcd(24,36))
+    p = 61
+    q = 53
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = 5
+    print(f"n is {n}, phi is: {phi}, e is: {e}")
+    g, d, y = gcdExtended(e,phi)
+    print(g, d, y)
+
+    print(f"Private key: {e, n}, Purblic key: {d, n}")
+
+    msg=1337
+    C=pow(msg,e)%n
+    print(f"msg is {msg}, C is {C}")
+
+    M=pow(C,d)%n
+    print(f"msg is {msg}, M is {M}")
 
 if __name__ == "__main__":
     main()
