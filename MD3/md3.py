bf1aba2ea64305e22bd9bed7c9d756e258bec146
krjohnn <ks91ko@gmail.com>
2025-12-06 00:52:02 +0200

    Adding GCDs

--- MD3/md3.py
+++ MD3/md3.py
@@ -1,16 +1,25 @@
 import hashlib
 
 def gcd(a, b):
-    return
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 def gcdExtended(a, b):
-    return
+    if a == 0:
+        return b, 0, 1
+    gcd, x1, y1 = gcdExtended(b % a, a)
+    x = y1 - (b // a) * x1
+    y = x1
+    return gcd, x, y
 
 def generate_keys(p, q):
     return
 
 def main():
     print("Start")
+    print(gcd(24,36))
+    print(gcdExtended(24,36))
 
 if __name__ == "__main__":
     main()
